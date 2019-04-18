b1fc64eb4fc326871c756a760f76fbfa3bccea6a
sen.shao <shaos91@126.com>
2019-04-18 10:52:22 +0800

    HelloWorld

---
+++ demo_01/src/main/java/controller/SpringbootControllerApplication.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootControllerApplication.class, args);
+    }
+}
